13451e1470614739a9739024014806509a2e2719
Your Name <you@example.com>
2021-11-17 12:31:15 -0800

    edited wk 7 hw

--- scripts/week_7_homework.R
+++ scripts/week_7_homework.R
@@ -1,11 +1,15 @@
-#libary(tidyverse)
-#library(ggthemes)
-#getwd()
+library(tidyverse)
+library(ggthemes)
+library(ggplot2)
+getwd()
 
 gapminder <- read_csv("https://gge-ucd.github.io/R-DAVIS/data/gapminder.csv")
 
 
-##1. To get the population difference between 2002 and 2007 for each country, it would probably be easiest to have a country in each row and a column for 2002 population and a column for 2007 population.
+##1. To get the population difference between 2002 and 2007 for each country, 
+#it would probably be easiest to have a country in each 
+#row and a column for 2002 population and a column for 
+#2007 population.
 gapminder %>%
   filter(year %in% c(2002, 2007))
 
@@ -26,7 +30,8 @@ gapminder %>% filter(year %in% c(2002,2007)) %>%
 
 new_data = gapminder %>% filter(year %in% c(2002,2007)) %>%
   pivot_wider(id_cols = c(country,continent),names_from = year,values_from = pop) %>%
-  mutate(popDiff = `2007`-`2002`) %>%#use these carrot things-- so you don't do math with things that arent numbers
+  mutate(popDiff = `2007`-`2002`) %>%#use these carrot things-- so you don't do 
+  #math with things that aren't numbers
   filter(continent!='Oceania') 
 
 ggplot(new_data) + facet_wrap(~continent) +
@@ -36,21 +41,21 @@ ggplot(new_data) + facet_wrap(~continent) +
 ggplot(new_data) + geom_bar(aes(x = continent))
 
 ggplot(data =new_data) + facet_wrap(~continent) +
-  geom_bar(aes(x = reorder(country,popDiff),y = popDiff),stat = 'identity') #tells R not to do anything with stat but what we tell it
+  geom_bar(aes(x = reorder(country,popDiff),y = popDiff), stat = 'identity') #tells R not to do anything with stat but what we tell it
 
 ggplot(data =new_data) + facet_wrap(~continent,scales = 'free') +
-  geom_bar(aes(x = reorder(country,popDiff),y = popDiff),stat = 'identity') +
-  labs(x = 'Country',y = 'change in pop. 2002 to 2007') + 
-  theme_bw() +
-  theme(axis.text.x = element_text(colour = 'red',angle = 45)) +
-  coord_flip()
+  geom_bar(aes(fill = continent, x = reorder(country,popDiff),y = popDiff),stat = 'identity') + 
+  labs(x = 'Country',y = 'change in pop. 2002 to 2007') +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
-
+##I couldn't figure out how to change the colors on these graphs 
+  
 
 ##2. Notice the order of countries within each facet. You’ll have to look up how to order them in this way.
 
 
 ##3. Also look at how the axes are different for each facet. Try looking through ?facet_wrap to see if you can figure this one out.
+?facet_wrap
 
 ##4. The color scale is different from the default- feel free to try out other color scales, just don’t use the defaults!
   
@@ -59,3 +64,7 @@ ggplot(data =new_data) + facet_wrap(~continent,scales = 'free') +
 ##6. The axis labels are rotated! Here’s a hint: angle = 45, hjust = 1. It’s up to you (and Google) to figure out where this code goes!
   
 ##7. Is there a legend on this plot?
+ggplot(data =new_data) + facet_wrap(~continent,scales = 'free') +
+  geom_bar(aes(fill = continent, x = reorder(country,popDiff),y = popDiff),stat = 'identity') + 
+  labs(x = 'Country',y = 'change in pop. 2002 to 2007') +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) + theme (legend.position = "none")
