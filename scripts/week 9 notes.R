57ad132e9de371918b37e70ab652eb454ea05914
Your Name <you@example.com>
2021-11-18 16:00:31 -0800

    Edited wk 9 notes/homework

--- scripts/week 9 notes.R
+++ scripts/week 9 notes.R
@@ -152,4 +152,153 @@ challenge_2 <- function(cntry2, dat = gapminder) {
     geom_line()
       return(plot)
 }
-challenge_2("Zambia")  
+challenge_2("Zambia") 
+
+##------WEEK 9 HOMEWORK-------##
+library(tidyverse)
+library(lubridate)
+mloa <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
+
+mloa
+
+#Use the README file associated with the Mauna Loa dataset to determine 
+#in what time zone the data are reported, and how missing values are 
+#reported in each column.
+
+#copy and paste this into the search bar
+#https://github.com/gge-ucd/R-DAVIS/blob/master/data/mauna_loa_README.txt
+
+#1
+#With the mloa data.frame, remove observations with missing values in rel_humid,
+#temp_C_2m, and windSpeed_m_s.
+
+mloa_sub = mloa %>% filter(rel_humid!=-99,temp_C_2m!=-999.9,windSpeed_m_s!=-99.9)
+
+#2
+#Generate a column called “datetime” using the year, month, day, hour24, and min columns. 
+
+#Next, create a column called “datetimeLocal” that converts the datetime
+#column to Pacific/Honolulu time (HINT: look at the lubridate function 
+#called with_tz()). Then, use dplyr to calculate the mean hourly 
+#temperature each month using the temp_C_2m column and the datetimeLocal
+#columns. (HINT: Look at the lubridate functions called month() and 
+#hour()). Finally, make a ggplot scatterplot of the mean monthly 
+#temperature, with points colored by local hour.
+
+# this version pastes year month day together by row and then calls ymd
+#sep=what is separating the things
+#paste function will push strings together; it can help us separate things with a space or dash mark
+?paste
+
+mloa_sub %>% mutate(datetime = paste(year,month,day,sep = '-')) %>%
+  mutate(datetime = ymd(datetime)) %>% select(datetime)
+
+#this version does both actions at once in the same mutate call
+mloa_sub %>% mutate(datetime = ymd(paste(year,month,day,sep = '-'))) %>% select(datetime)
+
+#now let's add in hour24...
+mloa_sub %>% mutate(datetime = ymd_h(paste(year,month,day,hour24,sep = '-'))) %>% select(datetime)
+
+
+#now let's add in min..
+mloa_sub %>% mutate(datetime = ymd_hm(paste(year,month,day,hour24,min,sep = '-'))) %>% select(datetime)
+
+#now let's add in min, and we are done with this task
+mloa_sub = mloa_sub %>% mutate(datetime = ymd_hm(paste(year,month,day,hour24,min,sep = '-')))
+
+mloa_sub
+
+#Next, create a column called “datetimeLocal” that converts the datetime
+#column to Pacific/Honolulu time (HINT: look at the lubridate function 
+#called with_tz()). 
+?with_tz()
+
+mloa_sub$datetimeLocal  = with_tz(time = mloa_sub$datetime,tzone = 'Pacific/Honolulu')
+##mloa_sub-- use $ to call column datetimeLocal- then use with_tz
+
+#Then, use dplyr to calculate the mean hourly temperature each month 
+#using the temp_C_2m column and the datetimeLocal
+#columns. (HINT: Look at the lubridate functions called month() and 
+#hour()). 
+
+mloa_sub %>% group_by()
+##Alycia this is where I left off in class-- not finished
+
+#Finally, make a ggplot scatterplot of the mean monthly 
+#temperature, with points colored by local hour.
+
+#-----PART 2 OF LECTURE------ Writing functions
+
+avgGDP(cntry = "Iran", yr.range = 1985:1990)#character data needs quotations
+avgGDP(cntry = "Zambia", yr.range = 1900:2020)
+
+##iteration
+
+# What if I want to do it over and over again, for every country?
+unique(gapminder$country)
+
+#Often times when we want to iterate, it can't be vectorized
+avgGDP(cntry = unique(gapminder$country)[1:10], yr.range = 1985:1990)
+##this doesn't work-- error about object length, we need to try something else
+
+# Iterating for 'for-loops'
+for(i in 1:10){
+  print(i)
+}
+
+# But, items within functions are 'pass by' values and don't save to the environment. Often don't even return
+# Could start by printing within loops
+for(i in unique(gapminder$country)){
+  print(avgGDP(cntry = i, yr.range = 1985:1990))
+}
+##think of this as copying and pasting each country 
+
+#What if you want it saved? these do not work
+output <- for(i in unique(gapminder$country)[1:10]){
+  print(avgGDP(i, yr.range = 1985:1990))
+}
+output
+#this doesn't work in for-loops
+
+for(i in unique(gapminder$country)[1:10]){
+  output <- avgGDP(i, yr.range = 1985:1990)
+}
+output
+#this is overwriting itself 10 times so we only get the last country
+
+#the pass by feature means it will only save the object for the last value of i
+
+# Create an empty container outside the loop, and assign it to an indexed value within the loop
+output <- list()
+for(i in unique(gapminder$country)[1:10]){
+  output[i] <- avgGDP(i, yr.range = 1985:1990)
+}
+#output[i] means that each iteration will not overwrite itself
+
+output
+do.call(rbind, output)
+data.frame(unlist(output))
+
+# Could also make this a data frame, but dataframes are more rigid about columns, but this can be useful if
+output <- data.frame()
+for(i in unique(gapminder$country)[1:10]){
+  df <- data.frame(country = i,
+                   output = avgGDP(i, yr.range = 1985:1990))
+}
+
+#challenge 
+#Write a new function that takes two arguments, the gapminder data.frame
+#(d) and the name of a country (e.g. "Afghanistan"), and plots a time 
+#series of the country’s population. That is, the return value from the 
+#function should be a ggplot object. - It is often easier to modify 
+#existing code than to start from scratch. To start out with, try
+#writing some code that works interactively, and then wrap it in a 
+#function.
+
+library(gapminder)
+d <- gapminder::gapminder
+
+for(i in unique(gapminder$country){
+  output[i]= <- 
+} 
+#pick up here in 2 weeks...
